fb41ce6fe1a42fcf8bd90e3078b74578519e6aa5
Kieran Walsh <kieranawalsh@gmail.com>
2017-01-25 13:21:44 +0000

    Added first version of the forename surname add-on script.

---
+++ Add-ADUsersForenameSurname/Add-ADUsersForenameSurname.ps1
@@ -0,0 +1,24 @@
+﻿$users = Import-Csv -Path 'C:\SRV\AD Usernames.csv'
+$couldNotFind = @()
+
+foreach($user in $users)
+{
+    "Configuring $($user.username)"
+    try
+    {
+        $null = Get-ADUser $user.username -ErrorAction Stop
+        Set-ADUser -Identity $user.username -GivenName $user.Forename -Surname $user.Surname
+    }
+    catch
+    {
+        $couldNotFind += $user.Username
+    }
+}
+
+'Script complete.'
+If($couldNotFind)
+{
+    ' '
+    'The following users were not found in Active Directory:'
+    $couldNotFind
+}
